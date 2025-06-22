2dd02d6a9b53654757a1adc2a03355590b551c01
Mohammed-bintaleb <engineerbintaleb@gmail.com>
2025-06-22 11:16:31 +0300

    Create Featured Books List View & add to home view body

---
+++ lib/Features/home/presentation/views/widgets/featured_list_view_item.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/assets.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        height: 100,
+        width: MediaQuery.of(context).size.width * 0.25,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.red,
+          image: const DecorationImage(
+            image: AssetImage(AssetsData.testImage),
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-
 import 'custom_app_bar.dart';
-import 'custom_list_view_item.dart';
+import 'featured_books_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [CustomAppBar(), CustomListViewItem()]);
+    return Column(children: [CustomAppBar(), FeaturedBooksListView()]);
   }
 }

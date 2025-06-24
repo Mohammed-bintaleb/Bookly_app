f183d4d7677e67e6dfea0d96ab2ee0712048eb72
Mohammed-bintaleb <engineerbintaleb@gmail.com>
2025-06-24 16:48:49 +0300

    Cached network image

--- lib/Features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -8,17 +9,10 @@ class CustomBookImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2.7 / 4,
-      child: Container(
-        height: 100,
-        width: MediaQuery.of(context).size.width * 0.25,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: Colors.red,
-          image: DecorationImage(
-            image: NetworkImage(imageUrl),
-            fit: BoxFit.fill,
-          ),
-        ),
+      child: CachedNetworkImage(
+        fit: BoxFit.fill,
+        imageUrl: imageUrl,
+        errorWidget: (context, url, error) => const Icon(Icons.error),
       ),
     );
   }
